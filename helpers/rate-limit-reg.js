f7fa1402f03bf055c18f7da0d341105f2312ccf9
Anna Bychkova <bychkova.dev@gmail.com>
2021-03-22 00:02:10 +0200

    gravatar installment

--- helpers/rate-limit-reg.js
+++ helpers/rate-limit-reg.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 const { HttpCode } = require("./constants");
 const createAccountLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 15 minutes
-  max: 2,
+  max: 100,
   handler: (req, res, next) => {
     return res.status(HttpCode.BAD_REQUEST).json({
       // status: "error",
